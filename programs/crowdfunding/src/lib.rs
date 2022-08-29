1240c0707c9b96d72ba34fa6c48b48628d5a75c3
Salih Poovathingal Hussainar <salihpoovathingalhussainar@Salihs-MacBook-Air.local>
2022-08-29 20:38:44 +0530

    contract deployed on devnet

--- programs/crowdfunding/src/lib.rs
+++ programs/crowdfunding/src/lib.rs
@@ -1,9 +1,11 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8H7wt8CR8ifx7PDVikrUZALGH1NZKKFXkc29FM3zmeNS");
 
 #[program]
 pub mod crowdfunding {
+
     use super::*;
 
     pub fn create(ctx: Context<Create>, name: String, description: String) -> Result<()> {
@@ -14,12 +16,62 @@ pub mod crowdfunding {
         campaign.admin = *ctx.accounts.user.key;
         Ok(())
     }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        let user = &mut ctx.accounts.user;
+        if campaign.admin != *user.key {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+        let rent_balance = Rent::get()?.minimum_balance(campaign.to_account_info().data_len());
+        if **campaign.to_account_info().lamports.borrow() - rent_balance < amount {
+            return Err(ProgramError::InsufficientFunds);
+        }
+
+        **campaign.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **user.to_account_info().try_borrow_mut_lamports()? += amount;
+
+        Ok(())
+    }
+
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> Result<()> {
+        let tx = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.user.key(),
+            &ctx.accounts.campaign.key(),
+            amount,
+        );
+
+        anchor_lang::solana_program::program::invoke(
+            &tx,
+            &[
+                ctx.accounts.user.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ],
+        );
+        (&mut ctx.accounts.campaign).amount_donated += amount;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
-
 pub struct Create<'info> {
     #[account(init,payer=user,space=9000,seeds=[b"CAMPAIN_DEMO".as_ref(),user.key().as_ref()],bump)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut)]
+    pub campaign: Account<'info, Campaign>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+#[derive(Accounts)]
+pub struct Donate<'info> {
+    #[account(mut)]
     pub campaign: Account<'info, Campaign>,
     #[account(mut)]
     pub user: Signer<'info>,
