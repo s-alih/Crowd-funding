0bfa2870c10c0d638f852029cd13823a2d7f02b4
Salih Poovathingal Hussainar <salihpoovathingalhussainar@Salihs-MacBook-Air.local>
2022-08-30 23:09:32 +0530

    creating campaign

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,17 +9,19 @@ import {
 	utils,
 	BN,
 } from "@project-serum/anchor";
+import {Buffer} from 'buffer';
+window.Buffer =  Buffer;
 
-import {Buffer} from "buffer";
-window.Buffer = Buffer;
+const programId = new PublicKey(idl.metadata.address);
+const network = clusterApiUrl("devnet"); 
+const opts = {
+ preflightCommitment: "processed",
+}
 
 function App() {
   const [walletAddress,setWalletAddress] = useState(null);
-   const programId = new PublicKey(idl.metadata.address);
-   const network = clusterApiUrl("devnet"); 
-   const opts = {
-    preflightCommitment: "processed",
-   }
+  const [campaigns, setCampaings] = useState([]);
+
 
   const { SystemProgram} = web3
   const getProvider = async() => {
@@ -28,14 +30,30 @@ function App() {
     return provider;
   }
 
+  const getCampaign = async() => {
+    try{
+      const connection = new Connection(network,opts.preflightCommitment);
+      const provider = await getProvider();
+      const program = new Program(idl,programId,provider);
+      Promise.all((await connection.getProgramAccounts(programId)).map(async (campaign)=>({
+        ...(await program.account.campaign.fetch(campaign.pubkey)), 
+        pubkey: campaign.pubkey,
+      })) ).then((campaigns) => setCampaings(campaigns));  
+      console.log(campaign);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
   const campaign = async() => {
     try{
       const provider = await getProvider();
     const program = new Program(idl,programId,provider);
     const [campaign] = await PublicKey.findProgramAddressSync([
       utils.bytes.utf8.encode("CAMPAIN_DEMO"),
-      provider.wallet.PublicKey.toBuffer(),
+      provider.wallet.publicKey.toBuffer(),
     ],program.programId);
+    console.log(provider.wallet.publicKey.toString())
     await program.rpc.create("Billionaire","Airdroping billions",{accounts:{
       campaign,
       user:provider.wallet.publicKey,
@@ -43,9 +61,9 @@ function App() {
     }});
 
     console.log("Created a new campaign")
-    console.log(campaign);
+    console.log(campaign.toString());
     }catch(err){
-      console.log(err);
+      console.log("Error creating campaign account",err);
     }
     
   }
@@ -77,8 +95,11 @@ function App() {
       if(solana){
         if(solana.isPhantom){
           console.log("phatom wallet found");
-          await  solana.connect()
+         const response = await  solana.connect()
+         setWalletAddress(response.publicKey.toString());
         }
+       
+
       }else{
         alert("Please connect you phantom wallet");
       }
@@ -92,7 +113,20 @@ function App() {
   );
 
   const renderConnectedComponent = () => (
-    <button onClick={campaign}>Create a campaign</button>
+    <>
+        <button onClick={campaign}>Create a campaign</button>
+        <button onClick={getCampaign}>Get Campaigns</button>
+        <br />
+        {campaigns.map((campaign) => (
+          <>
+          <p>Program Id: {campaign.pubkey.toString()}</p>
+          <p>Amount: {(campaign.amountDonated/web3.LAMPORTS_PER_SOL).toString()}</p>
+          <p>{campaign.name}</p>
+          <p>{campaign.description}</p>
+          <br />
+          </>
+        ),)}
+    </>
  );
 
   useEffect(() => {
@@ -103,7 +137,7 @@ function App() {
     return () => window.removeEventListener("load", onLoad);
   }, []);
 
-  return <div className="App"><h1>{!walletAddress && renderNotConnectedComponent()}{walletAddress && renderConnectedComponent}</h1></div>;
+  return <div className="App"><h1>{!walletAddress && renderNotConnectedComponent()}{walletAddress && renderConnectedComponent()}</h1></div>;
 }
 
 export default App;
